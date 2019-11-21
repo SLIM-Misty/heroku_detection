e3761873620329034590c5b7dcf75a9ae744d176
Hunter Thompson <hthompson@a10networks.com>
2019-11-21 23:34:43 +0000

    Removed unused reqs

--- object_detection.py
+++ object_detection.py
@@ -30,7 +30,6 @@ from PIL import Image
  
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
-from object_detection.utils import ops as utils_ops
  
 if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
@@ -42,7 +41,7 @@ if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
 # Here are the imports from the object detection module.
  
 from utils import label_map_util
- 
+from utils import ops as utils_ops 
 from utils import visualization_utils as vis_util
  
  
