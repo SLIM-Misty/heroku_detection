c9ab72e1e4d05edeb2c64dc096642f8c7e00cea2
Hunter Thompson <hthompson@a10networks.com>
2019-11-21 23:09:08 +0000

    Added procfile and reqs

---
+++ Procfile
@@ -0,0 +1,2 @@
+python: pip3 install -r requirements.txt
+object: python3 object_detection.py
--- object_detection.py
+++ object_detection.py
@@ -360,7 +360,7 @@ async def consumer_handler(websocket, path):
     print('recvd messg')
     await process_image(websocket, message)
  
-start_server = websockets.serve(consumer_handler, "localhost", 8765)
+start_server = websockets.serve(consumer_handler, "0.0.0.0", 8765)
 print('Websocket listening on port 8765!')
  
 asyncio.get_event_loop().run_until_complete(start_server)
